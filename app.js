557a350c023dc1c95e2ff0290f92d1c3492e73c2
Kristians Artis Berzins <kristiansberzins@outlook.lv>
2021-03-09 15:52:31 +0200

    Adjusted after lesson #11

--- app.js
+++ app.js
@@ -1,8 +1,26 @@
 const list = document.querySelector('#book-list ul');
 
-list.addEventListener('click', function(e){
-    if(e.target.className == 'delete'){
-        const li = e.target.parentElement;
-        list.removeChild(li);
-    }
+// delete books
+list.addEventListener('click', (e) => {
+  if(e.target.className == 'delete'){
+    const li = e.target.parentElement;
+    li.parentNode.removeChild(li);
+  }
+});
+
+const forms = document.forms;
+console.log(forms);
+console.log(forms['add-book']);
+
+Array.from(forms).forEach(function(form){
+  console.log(form);
+});
+
+// add books to the list
+
+const addForm = forms['add-book'];
+addForm.addEventListener('submit', function(e){
+  e.preventDefault();
+  const value = addForm.querySelector('input[type="text"]').value;
+  console.log(value);
 });
