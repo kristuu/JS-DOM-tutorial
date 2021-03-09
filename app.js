f9872c71c6e049c91edcc4bbeabd3bdced938590
Kristians Artis Berzins <kristiansberzins@outlook.lv>
2021-03-09 14:46:48 +0200

    Adjusted according to lesson #5

--- app.js
+++ app.js
@@ -1,12 +1,10 @@
-const wmf = document.querySelector('#book-list li:nth-child(2) .name');
-console.log(wmf);
+var books = document.querySelectorAll('#book-list li .name');
 
-var books = document.querySelector('#book-list li .name');
-console.log(books);
-
-books = document.querySelectorAll('#book-list li .name');
-console.log(books);
-
-Array.from(books).forEach(function(book){
-    console.log(book);
+// books is a NodeList (querySelectorAll converts a list to NodeList), which does not have to be converted with Array.from(books), but can be
+books.forEach(function(book){
+    book.textContent += ' (book title)';
 });
+
+const bookList = document.querySelector('#book-list');
+bookList.innerHTML = '<h2>Books and more books...</h2>';
+bookList.innerHTML += '<p>This is how you add HTML</p>'
