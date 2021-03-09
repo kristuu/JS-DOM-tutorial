3b3bd7336a33d8947fd16d636e17a284a550ca5d
Kristians Artis Berzins <kristiansberzins@outlook.lv>
2021-03-09 16:05:39 +0200

    Adjusted after lesson #12

--- app.js
+++ app.js
@@ -22,5 +22,19 @@ const addForm = forms['add-book'];
 addForm.addEventListener('submit', function(e){
   e.preventDefault();
   const value = addForm.querySelector('input[type="text"]').value;
-  console.log(value);
+
+  // create elements
+  const li = document.createElement('li');
+  const bookName = document.createElement('span');
+  const deleteBtn = document.createElement('span');
+
+  // add content
+  deleteBtn.textContent = 'delete';
+  bookName.textContent = value;
+
+  // append to DOM
+  li.appendChild(bookName);
+  li.appendChild(deleteBtn);
+  list.appendChild(li);
+
 });
