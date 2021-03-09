b665aa9a67cb87a02f11f5075d45a3cc031f301f
Kristians Artis Berzins <kristiansberzins@outlook.lv>
2021-03-09 16:12:56 +0200

    Adjusted after lesson #13

--- app.js
+++ app.js
@@ -32,6 +32,10 @@ addForm.addEventListener('submit', function(e){
   deleteBtn.textContent = 'delete';
   bookName.textContent = value;
 
+  // add classes
+  bookName.classList.add('name');
+  deleteBtn.classList.add('delete');
+
   // append to DOM
   li.appendChild(bookName);
   li.appendChild(deleteBtn);
